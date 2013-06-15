c0d8f2c08f1ea17a5168c48f1e900f2edbdd478a
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2013-06-15 20:33:36 +1000

    #4 Implemented ForeignKeyInitializer hook.

---
+++ src/FakeDb.Tests/Builtin/MaterializationHooks/ForeignKeyInitializerFacts.cs
@@ -0,0 +1,42 @@
+﻿using FakeDb.Builtin.MaterializationHooks;
+using Xunit;
+
+namespace FakeDb.Tests.Builtin.MaterializationHooks
+{
+    public class ForeignKeyInitializerFacts
+    {
+        public class ExecuteMethod
+        {
+            [Fact]
+            public void SetsForeignKey()
+            {
+                var message = new Message
+                    {
+                        Id = 1,
+                        Sender = new Sender
+                            {
+                                Id = 2
+                            }
+                    };
+
+                new ForeignKeyInitializer().Execute(message);
+
+                Assert.Equal(2, message.SenderId);
+            }
+        }
+
+        public class Message
+        {
+            public int Id { get; set; }
+
+            public Sender Sender { get; set; }
+
+            public int SenderId { get; set; }
+        }
+
+        public class Sender
+        {
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ src/FakeDb/Builtin/MaterializationHooks/ForeignKeyInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace FakeDb.Builtin.MaterializationHooks
+{
+    public class ForeignKeyInitializer : IMaterializationHook
+    {
+        public void Execute(object @object)
+        {
+            var properties = @object.GetType().GetProperties(ReflectionSettings.AllInstance);
+
+            foreach (var property in properties)
+            {
+                if (!property.PropertyType.IsClass)
+                    continue;
+
+                var fkProperty = properties.SingleOrDefault(p => p.Name == property.Name + "Id");
+
+                if (fkProperty == null)
+                    continue;
+
+                var relatedObject = property.GetValue(@object, null);
+
+                var idProperty = relatedObject.GetType().GetProperty("Id");
+
+                if (idProperty == null)
+                    continue;
+
+                if (!fkProperty.PropertyType.IsAssignableFrom(idProperty.PropertyType))
+                    continue;
+
+                var id = idProperty.GetValue(relatedObject, null);
+
+                fkProperty.SetValue(@object, Convert.ChangeType(id, fkProperty.PropertyType), null);
+            }
+        }
+    }
+}
