9151593f58d09cff6ceb959584045030e033783f
dragonpig <mikezhuyuan@hotmail.com>
2013-06-16 16:23:39 +1000

    Added foreign key mapping related unit tests.

--- src/FakeDb.Tests/FakeDbFacts.cs
+++ src/FakeDb.Tests/FakeDbFacts.cs
@@ -16,8 +16,25 @@ namespace FakeDb.Tests
                 var car = new Car();
                 db.Set<Car>().Add(car);
 
-                Assert.True(car.CarId > 0);
+                Assert.True(car.CarId != 0);
                 Assert.True(car.Id == 0);
+            }
+        }
+
+        public class MapForeignKey
+        {
+            [Fact]
+            public void CanMapForeignKey()
+            {
+                var db = new Db();
+                db.RegisterForeignKeyInitializer();
+                db.MapForeignKey((Person c) => c._AddressId, c => c.Address);
+
+                var p = new Person {Address = new Address()};
+
+                db.Set<Person>().Add(p);
+                Assert.NotNull(p._AddressId);
+                Assert.Null(p.AddressId);
             }
         }
     }
---
+++ src/FakeDb.Tests/ForeignKeyFinderFacts.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace FakeDb.Tests
+{
+    public class ForeignKeyFinderFacts
+    {
+        public class FindMethod
+        {
+            [Fact]
+            public void ReturnsDefaultFkProperty()
+            {
+                var finder = new ForeignKeyFinderFinder();
+                var prop = finder.Find(typeof (Person).GetProperty("Address"));
+
+                Assert.Equal("AddressId", prop.Name);
+            }
+
+            [Fact]
+            public void ReturnsRegisteredFkProperty()
+            {
+                var finder = new ForeignKeyFinderFinder();
+                finder.RegisterForeignKey(typeof(Person), "_AddressId", "Address");
+
+                Assert.Equal("_AddressId", finder.Find(typeof(Person).GetProperty("Address")).Name);
+            }
+        }
+    }
+}
