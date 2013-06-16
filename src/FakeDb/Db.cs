4a6b373bc81174f47c14cf41911e736c5fe1d059
dragonpig <mikezhuyuan@hotmail.com>
2013-06-16 16:23:04 +1000

    Updated FakeDb.Db to support foreign key mapping.

--- src/FakeDb/Db.cs
+++ src/FakeDb/Db.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using FakeDb.Builtin.MaterializationHooks;
 
 namespace FakeDb
 {
@@ -9,12 +10,18 @@ namespace FakeDb
     {
         readonly ICache _cache;
         readonly IIdPropertyFinder _idPropertyFinder;
+        readonly ForeignKeyFinderFinder _foreignKeyFinder;
+        readonly IList<IMaterializationHook> _materializationHooks;
 
         public Db(IIdGenerator idGenerator = null, IEnumerable<IMaterializationHook> materializationHooks = null)
         {
             _idPropertyFinder = new IdPropertyFinder();
-            _cache = new Cache(idGenerator ?? new IdGenerator(_idPropertyFinder), new ObjectGraph(),
-                               materializationHooks ?? Enumerable.Empty<IMaterializationHook>());
+            _foreignKeyFinder = new ForeignKeyFinderFinder();
+            _materializationHooks = materializationHooks == null
+                                        ? new List<IMaterializationHook>()
+                                        : materializationHooks.ToList();
+
+            _cache = new Cache(idGenerator ?? new IdGenerator(_idPropertyFinder), new ObjectGraph(), _materializationHooks);
         }
 
         public IInMemorySet Set<TEntity>() where TEntity : class
@@ -22,23 +29,42 @@ namespace FakeDb
             return _cache.For(typeof (TEntity));
         }
 
-        public Db MapId<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> idPropExpr) 
-            where TEntity: class 
-            where TProperty : struct
+        public void RegisterForeignKeyInitializer()
         {
-            string idPropName;
-            try
-            {
-                idPropName = ((MemberExpression) idPropExpr.Body).Member.Name;
-            }
-            catch(Exception ex)
-            {
-                throw new ArgumentException("Invalid property expression. Please use the form as 'object.objId'", ex);
-            }
+            _materializationHooks.Add(new ForeignKeyInitializer(_idPropertyFinder, _foreignKeyFinder));
+        }
 
+        public Db MapId<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> idPropExpr) where TEntity: class 
+        {
+            var idPropName = GetPropertyNameFromExpression(idPropExpr);
+            
             _idPropertyFinder.RegisterIdName(typeof(TEntity), idPropName);
 
             return this;
+        }
+
+        public Db MapForeignKey<TEntity, TProperty, TForeignKey>(Expression<Func<TEntity, TForeignKey>> foeignKeyExpr, Expression<Func<TEntity, TProperty>> propExpr)
+            where TEntity : class
+            where TProperty : class
+        {
+            var propName = GetPropertyNameFromExpression(propExpr);
+            var fkName = GetPropertyNameFromExpression(foeignKeyExpr);
+
+            _foreignKeyFinder.RegisterForeignKey(typeof(TEntity), fkName, propName);
+
+            return this;
+        }
+
+        static string GetPropertyNameFromExpression<TEntity, TProperty>(Expression<Func<TEntity, TProperty>> expr)
+        {
+            try
+            {
+                return ((MemberExpression)expr.Body).Member.Name;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Invalid property expression. Please use the form of 'object.property'", ex);
+            }
         }
     }
 }
